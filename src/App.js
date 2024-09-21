fa3853f943d13dcd9184ca01247042507b79a8db
DHARM <opdsbanasya@gmail.com>
2024-09-22 00:30:18 +0530

    setup ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const App = () => {
+    return <>
+        <h1 className="text-5xl text-rose-600">NetflixGPT</h1>
+    </>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);

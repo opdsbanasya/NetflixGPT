5ca484fb93430bc134f07ae372d8460d802e4176
DHARM <opdsbanasya@gmail.com>
2024-09-24 18:31:57 +0530

    sign in & sign up form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import { MAIN_BG_IMG } from "../utils/constant";
 import Header from "./Header";
 
 const Login = () => {
+
+    const [formType, setFormType] = useState("signin");
+
+    const handleFormType = ()=>{
+        setFormType(formType === "signin" && "signup" || formType === "signup" && "signin")
+    }
     return (
-        <div className="relative ">
+        <section className="relative ">
             <Header />
             <div className="h-screen w-full overflow-hidden">
                 <img src={MAIN_BG_IMG} className="w-full object-cover object-center" alt="bg-img" />
             </div>
-        </div>
-    );
+            <div className="w-[32%] absolute text-white top-[38%] left-1/2 -translate-x-1/2 -translate-y-[38%] bg-black bg-opacity-85 py-10 px-16 space-y-10">
+                <h2 className=" text-3xl font-semibold">{formType === "signin" ? "Sign in":"Sign up"}</h2>
+                <form className="flex flex-col gap-5 ">
+                    {formType === "signup" && <input className="px-4 py-4 text-white outline-none border border-gray-400 rounded-md bg-black bg-opacity-50" type="text" placeholder="Full name" />}
+                    <input className="px-4 py-4 text-white outline-none border border-gray-400 rounded-md bg-black bg-opacity-50" type="email" placeholder="Email" />
+                    <input className="px-4 py-4 text-white outline-none border border-gray-400 rounded-md bg-black bg-opacity-50" type="password" placeholder="Password" />
+                    <input type="submit" value={formType === "signin" ? "Sign in":"Sign up"} className="py-2 px-4 text-white rounded-md font-semibold bg-red-700"/>
+                </form>
+                <h3>{formType === "signin" ? "New to Netflix?" : "Alraedy have account?"} <span onClick={()=> handleFormType()} className="font-semibold hover:underline cursor-pointer">Sign {formType === "signin" ? "up":"in"} now</span>.</h3>
+            </div>
+        </section>
+    );  
 }
 
 export default Login;

b82f2f399de94e2a42b540bf88f2734f03b322c2
DHARM <opdsbanasya@gmail.com>
2024-10-14 01:33:04 +0530

    movieCard

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import useMovies from "../hooks/useMovies";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
+import MovieCategories from "./MovieCategories";
 
 const Browse = () => {
 
@@ -16,6 +17,7 @@ const Browse = () => {
         <div>
             <Header />
             <MainContainer />
+            <MovieCategories />
         </div>
     );
 }
---
+++ src/Components/MovieCategories.js
@@ -0,0 +1,11 @@
+import MovieCards from "./MovieCards";
+
+const MovieCategories = () => {
+    return (
+        <div>
+            <MovieCards />
+        </div>
+    );
+}
+
+export default MovieCategories;

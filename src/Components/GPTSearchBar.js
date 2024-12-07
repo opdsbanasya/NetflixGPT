da1e4baa2b15070b4282d625c795455a871110c9
DHARM <opdsbanasya@gmail.com>
2024-12-08 02:51:31 +0530

    updation

--- src/Components/GPTSearchBar.js
+++ src/Components/GPTSearchBar.js
@@ -3,8 +3,9 @@ import React from 'react';
 const GPTSearchBar = () => {
     return (
         <div className='w-full'>
-            <form className='w-1/2 mx-auto'>
+            <form className='w-[60%] mx-auto flex gap-5'>
                 <input type='text' className='w-full mx-auto px-3 py-2 outline-none rounded-md text-black font-semibold text-lg' placeholder='What do you want to watch today?' />
+                <input type='submit' className='text-white font-semibold text-xl bg-purple-600 px-4 py-2 rounded-md cursor-pointer' value="Search" />
             </form>
         </div>
     );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const user = useSelector((store) => store.user)
+    const {showGPTButton} = useSelector(store => store.gptsearch);
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -43,7 +44,9 @@ const Header = () => {
         <header className="px-20 py-4 bg-black flex justify-between">
             <img className={`w-2/12 ${user && "w-[10%]"}`} src={NETFLIX_LOGO} alt="Netflix Logo" />
             {user && <nav className="flex items-center gap-8">
-                <button onClick={()=> handleGptSearch()} className="text-white font-semibold text-xl bg-purple-600 px-4 py-2 rounded-md cursor-pointer z-10">GPT Search</button>
+                <button onClick={()=> handleGptSearch()} className="text-white font-semibold text-xl bg-purple-600 px-4 py-2 rounded-md cursor-pointer z-10">
+                    {showGPTButton ? "Close GPT" : "GPT Search"}
+                </button>
                 <img className="w-8 rounded-full" src={user.photoURL} />
                 <button onClick={() => handleSignOut()} className="text-white font-semibold text-xl bg-red-600 px-4 py-2 rounded-md cursor-pointer">Sign out</button>
             </nav>}

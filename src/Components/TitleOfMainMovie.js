c967590ef4a25ca9acf5f480b4548ec07bc7346c
DHARM <opdsbanasya@gmail.com>
2024-12-15 22:43:49 +0530

    fixing description issue

--- src/Components/TitleOfMainMovie.js
+++ src/Components/TitleOfMainMovie.js
@@ -1,12 +1,12 @@
 
 const TitleOfMainMovie = ({title, overview}) => {
     return (
-        <div className="w-screen h-screen overflow-hidden absolute px-20 pt-[28%] text-white bg-gradient-to-r from-black bg-opacity-50 z-10">
+        <div className="w-screen h-screen overflow-hidden absolute px-20 pt-[23%] text-white bg-gradient-to-r from-black bg-opacity-50 z-10">
             <h2 className="text-4xl font-bold tracking-tight italic font-[cursive]">{title}</h2>
-            <p className="w-5/12 py-3 text-md">{overview}</p>
+            <p className="w-5/12 py-3 text-md">{overview.length <= 380 ? overview: overview.substring(0, 380)+"......"}</p>
             <div className="space-x-3">
                 <button className="px-4 py-2 bg-white text-black font-semibold rounded-md hover:bg-opacity-95 cursor-pointer">Play</button>
-                <button className="px-4 py-2 bg-gray-500 text-black font-semibold rounded-md hover:bg-gray-400 cursor-pointer">More Info</button>
+                <button className="px-4 py-2 bg-purple-500 text-white font-semibold rounded-md hover:bg-gray-400 cursor-pointer">More Info</button>
             </div>
         </div>
     );

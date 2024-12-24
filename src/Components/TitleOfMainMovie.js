36219b883547a970873e38f9c108ae8a979504b5
DHARM <opdsbanasya@gmail.com>
2024-12-24 23:55:16 +0530

    padding update

--- src/Components/TitleOfMainMovie.js
+++ src/Components/TitleOfMainMovie.js
@@ -1,7 +1,7 @@
 
 const TitleOfMainMovie = ({title, overview}) => {
     return (
-        <div className="w-screen h-screen overflow-hidden absolute px-20 pt-[23%] text-white bg-gradient-to-r from-black bg-opacity-50 z-10">
+        <div className="w-screen h-screen overflow-hidden absolute px-20 pt-[26%] text-white bg-gradient-to-r from-black bg-opacity-50 z-10">
             <h2 className="text-4xl font-bold tracking-tight italic font-[cursive]">{title}</h2>
             <p className="w-5/12 py-3 text-md">{overview.length <= 340 ? overview: overview.substring(0, 340)+"......"}</p>
             <div className="space-x-3">

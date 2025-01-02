f7e18c8c544a4b117373b5f2be62738b37b21ab7
DHARM <opdsbanasya@gmail.com>
2025-01-03 00:02:19 +0530

    Building Movie Page UI

--- src/Components/MoviePage.js
+++ src/Components/MoviePage.js
@@ -9,27 +9,30 @@ const MoviePage = () => {
     useMovieDetails(state);
 
     const { movieDetails } = useSelector( store => store?.moviedetail)
+    if(!movieDetails) return;
     console.log(movieDetails);
-    
+    const {title, original_language, 
+        revenue, status, release_date, genres, overview, poster_path
+    } = movieDetails;
     return (
         <div className='w-full bg-black text-white pt-20 '>
-            <article className='w-11/12 mx-auto px-10 py-5 flex items-start gap-8'>
+            <article className='w-11/12 min-h-[80vh] mx-auto px-10 py-5 flex items-start gap-8'>
                 <div className='w-3/12 h-[60vh] bg-zinc-50'>
-                    <image src="https://image.tmdb.org/t/p/w220_and_h330_face/lurEK87kukWNaHd0zYnsi3yzJrs.jpg" alt="Mufasa: The Lion King" />
+                    <img className='w-full' src={"https://image.tmdb.org/t/p/w220_and_h330_face" + poster_path} alt="Mufasa: The Lion King" />
                 </div>
-                <div className='w-7/12'>
-                    <h2 className='text-4xl font-bold'>Mufasa: The Lion King</h2>
+                <div className='w-7/12 space-y-3'>
+                    <h2 className='text-5xl font-serif font-bold mb-6'>{title}</h2>
                     <div className='flex gap-5'>
-                        <h5>language: en</h5>
-                        <h5>US</h5>
-                        <h5>2024-12-18</h5>
+                        <h5 className='bg-slate-700 px-2 py-[2px]'>lang: {original_language}</h5>
+                        <h5 className='bg-slate-700 px-2 py-[2px]'>{revenue}</h5>
+                        <h5 className='bg-slate-700 px-2 py-[2px]'>{status === "Released" ? release_date : "Upcoming"}</h5>
                     </div>
-                    <div >
-                        <h5>Adventure</h5>
-                        <h5>Animation</h5>
-                        <h5>Drama</h5>
+                    <div className='flex gap-5'>
+                        {genres.map((item) => (
+                            <h2 className='bg-slate-700 px-2 py-[2px]' key={item?.id}>#{item?.name}</h2>
+                        ))}
                     </div>
-                    <p>Told in flashbacks, Mufasa is an orphaned cub, lost and alone until he meets a sympathetic lion named Taka—the heir to a royal bloodline. The chance meeting sets in motion a journey of misfits searching for their destiny and working together to evade a threatening and deadly foe.</p>
+                    <p className='text-lg text-justify mt-5'>{overview}</p>
                     <button>Play</button>
                 </div>
             </article>

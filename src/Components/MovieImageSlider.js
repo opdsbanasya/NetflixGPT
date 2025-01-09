4c67ddb60ccfd35dc69bfd729b4bcdd9db25811f
DHARM SINGH <opdsbanasya@gmail.com>
2025-01-10 02:22:19 +0530

    movie screenshots

---
+++ src/Components/MovieImageSlider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MovieImageSlider = () => {
+    return (
+        <div className='w-11/12 min-h-[80vh] mx-auto px-20 py-5 bg-zinc-500'>
+            <h3 className='w-10/12 mx-auto text-2xl py-5'>Images</h3>
+            <div className='w-10/12 mx-auto h-full'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default MovieImageSlider

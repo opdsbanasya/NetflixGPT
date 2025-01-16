4c01149e67402bf925ae6098d1dd344127b6fd69
DHARM <opdsbanasya@gmail.com>
2025-01-17 01:09:21 +0530

    adding addMovieTrailer reducer

--- src/store/trailerSlice.js
+++ src/store/trailerSlice.js
@@ -3,19 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const trailerSlice = createSlice({
     name: "trailer",
     initialState: {
-        sliderTrailer: null
-        // movieTrailerVideo: null
+        sliderTrailer: null,
+        movieTrailerVideo: null
     },
     reducers: {
         addTrailerVideo: (state, action) => {
             state.sliderTrailer = action.payload;
+        },
+        addMovieTrailerVideo: (state, action) => {
+            state.movieTrailerVideo = action.payload;
         }
-        // addMovieTrailerVideo: (state, action) = {
-        //     state.movieTrailerVideo = action.payload,
-        // }
     }
 })
 
-export const { addTrailerVideo } = trailerSlice.actions;
+export const { addTrailerVideo, addMovieTrailerVideo } = trailerSlice.actions;
 
 export default trailerSlice.reducer;

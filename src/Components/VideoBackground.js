357baed3c38f51d3b33a3f3adcea038bb78d31fc
DHARM <opdsbanasya@gmail.com>
2025-01-17 01:21:55 +0530

    turn off caption

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -12,7 +12,7 @@ const VideoBackground = ({ movieId }) => {
         <div className="w-screen h-screen overflow-hidden">
             <iframe
             className="w-full h-full scale-125"
-            src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?autoplay=1&mute=1"}
+            src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?autoplay=1&mute=1&cc_load_policy=3"}
             title="YouTube video player"
             allow="encrypted-media; gyroscope; web-share" 
             ></iframe>

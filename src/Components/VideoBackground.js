387f200a693906cdc8a9b1a256652d138cf561d8
DHARM <opdsbanasya@gmail.com>
2025-01-17 01:56:36 +0530

    making Trailer component reusable

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -1,18 +1,18 @@
 import { useSelector } from "react-redux";
 import useMovieTrailer from "../hooks/useMovieTrailer";
 
-const VideoBackground = ({ movieId }) => {
+const VideoBackground = ({ movieId, isMoviePage }) => {
 
-    useMovieTrailer(movieId);
+    useMovieTrailer(movieId, isMoviePage);
 
-    const trailerVideo = useSelector(store => store.trailer?.sliderTrailer)
-
-
+    const trailerVideo = useSelector(store => isMoviePage ? store.trailer?.movieTrailerVideo : store.trailer?.sliderTrailer)
     return (
-        <div className="w-screen h-screen overflow-hidden">
+        <div className={`${isMoviePage ? "w-full h-full":"w-screen h-screen"}  overflow-hidden`}>
             <iframe
-            className="w-full h-full scale-125"
-            src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?autoplay=1&mute=1&cc_load_policy=3"}
+            className={`w-full h-full scale-125 ${isMoviePage && "aspect-video object-cover scale-90"}`}
+            src={isMoviePage ? "https://www.youtube.com/embed/" + trailerVideo?.key : 
+                "https://www.youtube.com/embed/" + trailerVideo?.key + "?autoplay=1&mute=1&cc_load_policy=3"
+            }
             title="YouTube video player"
             allow="encrypted-media; gyroscope; web-share" 
             ></iframe>

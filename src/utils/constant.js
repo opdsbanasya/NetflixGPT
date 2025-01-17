7101e63bac44b80a266df44423a2c14117f27449
DHARM <opdsbanasya@gmail.com>
2025-01-18 01:08:17 +0530

    adding

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,11 @@ export const MAIN_BG_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/47c2bc92-
 export const NETFLIX_LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 export const USER_ICON = "https://img.icons8.com/?size=100&id=13042&format=png&color=000000";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjA0N2Q0MTJlMGQ0YTlhY2JhNzQxMjI5M2M3MTcyNCIsIm5iZiI6MTcyNzAyODk1OC45MDMsInN1YiI6IjY2ZjA1ZWRlMGVhODBkMWZlMzhkODQyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GLE1W97QiNEmTI92ZFD8Ig4bRMFGk3zJ9E45XdxKQn4'
+    }
+};

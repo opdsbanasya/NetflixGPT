172ddb01fa27808dcedde39e4ec9bbb252f1e92d
DHARM <opdsbanasya@gmail.com>
2025-01-24 01:45:18 +0530

    making responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,15 +6,18 @@ import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../store/userSlice";
-import { NETFLIX_LOGO} from "../utils/constant";
+import { NETFLIX_LOGO } from "../utils/constant";
 import { toggleGptSearch } from "../store/gptSlice";
+import gptIcon from "../assets/gpt-search.png";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Header = () => {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const user = useSelector((store) => store.user)
-    const {showGPTButton} = useSelector(store => store.gptsearch);
+    const { showGPTButton } = useSelector(store => store.gptsearch);
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -30,10 +33,10 @@ const Header = () => {
     }, [])
 
     const handleSignOut = () => {
-        signOut(auth).then(() => {})
-        .catch((error) => {
-            console.log(error);
-        });
+        signOut(auth).then(() => { })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     const handleGptSearch = () => {
@@ -42,14 +45,15 @@ const Header = () => {
     }
 
     return (
-        <header className={`px-10 md:px-20 py-4 bg-transparent absolute top-0 ${user && "bg-black"}  flex justify-between`}>
-            <img className={`w-6/12 lg:w-2/12 mb-10 md:mb-0${user && "md:w-[10%]"}`} src={NETFLIX_LOGO} alt="Netflix Logo" />
-            {user && <nav className="flex items-center gap-8">
-                <button onClick={()=> handleGptSearch()} className="text-white font-semibold text-xl bg-purple-600 px-4 py-2 rounded-md cursor-pointer z-10">
-                    {showGPTButton ? "Close GPT" : "GPT Search"}
+        <header className={`px-8 md:px-20 bg-transparent absolute top-0 ${user && "bg-black"}  flex items-center justify-between`}>
+            <img className={`w-6/12 lg:w-2/12 py-4 ${user && "w-[35%] md:w-3/12"}`} src={NETFLIX_LOGO} alt="Netflix Logo" />
+            {user && <nav className="flex items-center gap-3 md:gap-8">
+                <button onClick={() => handleGptSearch()} className=" text-white font-semibold text-md md:text-xl px-2 md:px-4 md:py-2  cursor-pointer z-10">
+                    {!showGPTButton ? <img className="w-8 md:hidden" src={gptIcon} alt="GPT Search" /> : <IoCloseSharp className="text-2xl bg-red-600 rounded-md" />}
+                    <span className="hidden md:block px-4 py-2 text-xl bg-purple-600 rounded-md">{showGPTButton ? "Close GPT" : "GPT Search"}</span>
                 </button>
-                <img className="w-8 rounded-full" src={user.photoURL} />
-                <button onClick={() => handleSignOut()} className="text-white font-semibold text-xl bg-red-600 px-4 py-2 rounded-md cursor-pointer">Sign out</button>
+                <img className="w-8 rounded-full mr-1 md:mr-2" src={user.photoURL} />
+                <button onClick={() => handleSignOut()} className="text-white font-semibold md:text-xl bg-red-600 px-2 md:px-4 py-1 md:py-2 rounded-md cursor-pointer"><FaSignOutAlt /></button>
             </nav>}
         </header>
 

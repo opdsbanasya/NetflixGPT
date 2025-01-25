a85b7026699fc5bb129590ac2d2c272306d015b6
DHARM <opdsbanasya@gmail.com>
2025-01-26 02:59:42 +0530

    fixing text overlap

--- src/Components/TitleOfMainMovie.js
+++ src/Components/TitleOfMainMovie.js
@@ -5,7 +5,7 @@ const TitleOfMainMovie = ({title, overview}) => {
     return (
         <div className="w-screen h-[30vh] md:h-full overflow-hidden absolute px-5 mt-16 md:mt-0 md:px-20 pt-[29%] md:pt-[42%] lg:pt-[35%] text-white bg-gradient-to-r from-black bg-opacity-50 z-10 space-y-5">
             <h2 className="text-4xl font-bold tracking-tight italic font-[cursive]">{title}</h2>
-            <p className="w-5/12 py-3 text-xl hidden lg:block">{overview.length <= 300 ? overview: overview.substring(0, 300)+"......"}</p>
+            <p className="w-5/12 py-3 text-xl hidden lg:block">{overview.length <= 290 ? overview: overview.substring(0, 290)+"......"}</p>
             <div className="space-x-3">
                 <button className="px-2 py-1 md:px-4 md:py-2 text-sm bg-white text-black font-semibold rounded-md hover:bg-opacity-95 cursor-pointer">Play</button>
                 <button className="px-2 py-1 md:px-4 md:py-2 text-sm bg-purple-500 text-white font-semibold rounded-md hover:bg-gray-400 cursor-pointer">More Info</button>
